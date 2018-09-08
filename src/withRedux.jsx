a4ce9f0929e88042a7dbd9a52efda21b8d5b85ad
Drienn <Emitdutcher@gmail.com>
2018-09-08 08:53:52 -0700

    restarting project

---
+++ src/withRedux.jsx
@@ -0,0 +1,57 @@
+import React, { PureComponent } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionsBank } from 'src/redux/reducers';
+
+const notFoundInReduxMessage = field => console.error(
+  'withRedux Error: No Reducer found for',
+  `${field}.`,
+  'You may need to add the following to redux/reducers/index.js ~',
+  `import ${field}, * as ${field}Actions from './${field}';`,
+);
+
+const withRedux = (reducers, WrappedComponent, withState = true, withActions = true) => {
+  const HOC = class HOC extends PureComponent {
+    render() {
+      return <WrappedComponent {...this.props} />;
+    }
+  };
+
+  const mapState = (state) => {
+    const stateObj = {};
+
+    reducers.forEach((reducer) => {
+      if (actionsBank[reducer]) {
+        Object.assign(stateObj, { [reducer]: state[reducer] });
+      } else {
+        notFoundInReduxMessage(reducer);
+      }
+    });
+
+    return stateObj;
+  };
+
+  const mapDispatch = (dispatch) => {
+    const Actions = {};
+
+    reducers.forEach((reducer) => {
+      if (actionsBank[reducer]) {
+        const actionName = `${[reducer]}Actions`;
+        Object.assign(Actions, {
+          [actionName]: bindActionCreators(actionsBank[reducer], dispatch),
+        });
+      } else {
+        notFoundInReduxMessage(reducer);
+      }
+    });
+
+    return Actions;
+  };
+
+  return connect(
+    withState ? mapState : null,
+    withActions ? mapDispatch : {},
+  )(HOC);
+};
+
+export default withRedux;
