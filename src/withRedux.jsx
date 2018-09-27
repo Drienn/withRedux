c1bc023d8e5a32a1282615948a4c472c596ff1c7
Emit Dutcher <emit.dutcher@wearedauntless.com>
2018-09-26 20:21:37 -0700

    added ability to customize what props and actions are brought into components

--- src/withRedux.jsx
+++ src/withRedux.jsx
@@ -2,15 +2,21 @@ import React, { PureComponent } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 
-const notFoundInReduxMessage = field =>
-  console.error(
-    "withRedux Error: No Reducer found for",
-    `${field}.`,
-    "You may need to add the following to redux/reducers/index.js ~",
-    `import ${field}, * as ${field}Actions from './${field}';`
+const notFoundInReduxMessage = field => {
+  throw new Error(
+    `withRedux: No Reducer found for ${field}. You may need to add the following to redux/reducers/index.js ~ import ${field}, * as ${field}Actions from './${field}';`
   );
+};
 
-const withRedux = (reducers, actionsBank, WrappedComponent, withState = true, withActions = true) => {
+const stateNotFound = (stateName, reducer, component) => {
+  throw new Error(`withRedux State Error: No Key of "${stateName}" found in ${reducer}. Check the specific state requested of withRedux in ${component}`);
+};
+
+const actionNotFound = (actionName, reducer, component) => {
+  throw new Error(`withRedux Action Error: No Action called "${actionName}" found in ${reducer}. Check the specific actions requested of withRedux in ${component}`);
+};
+
+const withRedux = (reducers, actionsBank, WrappedComponent, specificState = true, specificActions = true) => {
   const HOC = class HOC extends PureComponent {
     render() {
       return <WrappedComponent {...this.props} />;
@@ -22,7 +28,22 @@ const withRedux = (reducers, actionsBank, WrappedComponent, withState = true, wi
 
     reducers.forEach(reducer => {
       if (actionsBank[reducer]) {
-        Object.assign(stateObj, { [reducer]: state[reducer] });
+        if (typeof specificState === "object" && specificState[reducer]) {
+          const currentWanted = specificState[reducer];
+
+          currentWanted.forEach(key => {
+            const { [key]: val } = state[reducer];
+            const wantedState = { [key]: val };
+
+            if (!{}.hasOwnProperty.call(state[reducer], key)) {
+              stateNotFound(key, reducer, `${WrappedComponent.name}`);
+            }
+
+            Object.assign(stateObj, { [reducer]: { ...stateObj[reducer], ...wantedState } });
+          });
+        } else {
+          Object.assign(stateObj, { [reducer]: state[reducer] });
+        }
       } else {
         notFoundInReduxMessage(reducer);
       }
@@ -37,9 +58,22 @@ const withRedux = (reducers, actionsBank, WrappedComponent, withState = true, wi
     reducers.forEach(reducer => {
       if (actionsBank[reducer]) {
         const actionName = `${[reducer]}Actions`;
-        Object.assign(Actions, {
-          [actionName]: bindActionCreators(actionsBank[reducer], dispatch)
-        });
+        if (typeof specificActions === "object" && specificActions[reducer]) {
+          const currentWanted = specificActions[reducer];
+
+          currentWanted.forEach(key => {
+            const { [key]: val } = actionsBank[reducer];
+            const wantedAction = { [key]: val };
+
+            if (typeof wantedAction[key] !== "function") actionNotFound(key, actionName, `${WrappedComponent.name}`);
+
+            Object.assign(Actions, { [actionName]: { ...Actions[reducer], ...wantedAction } });
+          });
+        } else {
+          Object.assign(Actions, {
+            [actionName]: bindActionCreators(actionsBank[reducer], dispatch)
+          });
+        }
       } else {
         notFoundInReduxMessage(reducer);
       }
@@ -49,8 +83,8 @@ const withRedux = (reducers, actionsBank, WrappedComponent, withState = true, wi
   };
 
   return connect(
-    withState ? mapState : null,
-    withActions ? mapDispatch : {}
+    specificState ? mapState : null,
+    specificActions ? mapDispatch : {}
   )(HOC);
 };
 
